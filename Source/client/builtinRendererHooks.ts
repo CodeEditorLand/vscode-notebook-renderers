343654efe9a056c7e8b5fbf4686a8096bc861464
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-30 12:20:11 +0200

    squash!

--- Source/client/builtinRendererHooks.ts
+++ Source/client/builtinRendererHooks.ts
@@ -6,54 +6,63 @@
 declare let __webpack_public_path__: string;
 declare const scriptUrl: string;
 const getPublicPath = () => {
-    return new URL(scriptUrl.replace(/[^/]+$/, '')).toString();
+	return new URL(scriptUrl.replace(/[^/]+$/, "")).toString();
 };
 
 // eslint-disable-next-line prefer-const
 __webpack_public_path__ = getPublicPath();
 
-import type { OutputItem, RendererContext } from 'vscode-notebook-renderer';
-if (!('$' in globalThis) && !('jQuery' in globalThis)) {
-    // Required by JS code.
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const jQuery = require('jquery');
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (globalThis as any).$ = jQuery;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (globalThis as any).jQuery = jQuery;
+import type { OutputItem, RendererContext } from "vscode-notebook-renderer";
+if (!("$" in globalThis) && !("jQuery" in globalThis)) {
+	// Required by JS code.
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const jQuery = require("jquery");
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	(globalThis as any).$ = jQuery;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	(globalThis as any).jQuery = jQuery;
 }
 export async function activate(ctx: RendererContext<void>) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const builtinRenderer = await ctx.getRenderer('vscode.builtin-renderer');
-    if (!builtinRenderer) {
-        throw new Error('Could not find the built-in js renderer');
-    }
-    try {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (builtinRenderer.experimental_registerHtmlRenderingHook as any)({
-            async postRender(_outputItem: OutputItem, element: HTMLElement, _signal: AbortSignal): Promise<undefined> {
-                // Output container is expected to have the class `output_html`
-                element.classList.add('output_html');
-                return;
-            }
-        });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (builtinRenderer.experimental_registerJavaScriptRenderingHook as any)({
-            async preEvaluate(
-                outputItem: OutputItem,
-                element: HTMLElement,
-                script: string,
-                _signal: AbortSignal
-            ): Promise<string | undefined> {
-                if (ctx.postMessage) {
-                    ctx.postMessage({ type: 'from Renderer', payload: 'Hello World' });
-                }
-                const metadata =
-                    outputItem.metadata && typeof outputItem.metadata === 'object' && 'metadata' in outputItem.metadata
-                        ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                          (outputItem.metadata as any)['metadata']
-                        : undefined;
-                return `
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const builtinRenderer = await ctx.getRenderer("vscode.builtin-renderer");
+	if (!builtinRenderer) {
+		throw new Error("Could not find the built-in js renderer");
+	}
+	try {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		(builtinRenderer.experimental_registerHtmlRenderingHook as any)({
+			async postRender(
+				_outputItem: OutputItem,
+				element: HTMLElement,
+				_signal: AbortSignal
+			): Promise<undefined> {
+				// Output container is expected to have the class `output_html`
+				element.classList.add("output_html");
+				return;
+			},
+		});
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		(builtinRenderer.experimental_registerJavaScriptRenderingHook as any)({
+			async preEvaluate(
+				outputItem: OutputItem,
+				element: HTMLElement,
+				script: string,
+				_signal: AbortSignal
+			): Promise<string | undefined> {
+				if (ctx.postMessage) {
+					ctx.postMessage({
+						type: "from Renderer",
+						payload: "Hello World",
+					});
+				}
+				const metadata =
+					outputItem.metadata &&
+					typeof outputItem.metadata === "object" &&
+					"metadata" in outputItem.metadata
+						? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+						  (outputItem.metadata as any)["metadata"]
+						: undefined;
+				return `
                 (function(){
                     let gotToUserScript = false;
                     try {
@@ -61,7 +70,9 @@ export async function activate(ctx: RendererContext<void>) {
                         // We're not fully supporting ipyvega yet, but this ensures the scripts will not fall over and will work with minimal effort on our part.
                         const context = {
                             outputs: [{
-                                metadata: ${JSON.stringify(metadata || {})}, data: {}
+                                metadata: ${JSON.stringify(
+									metadata || {}
+								)}, data: {}
                             }],
                         };
                         // Required by JS code in Jupyter notebook renderers again, even scenepic (Microsoft Python widget) uses this.
@@ -80,9 +91,9 @@ export async function activate(ctx: RendererContext<void>) {
                         }
                     }
                 })();`;
-            }
-        });
-    } catch (ex) {
-        throw new Error(`Failed to register JavaScript rendering hook: ${ex}`);
-    }
+			},
+		});
+	} catch (ex) {
+		throw new Error(`Failed to register JavaScript rendering hook: ${ex}`);
+	}
 }
--- Source/extension/index.ts
+++ Source/extension/index.ts
@@ -1,49 +1,69 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { Event, EventEmitter, ExtensionContext, notebooks, extensions, NotebookEditor } from 'vscode';
-import { OpenImageInPlotViewer, SaveImageAs, IsJupyterExtensionInstalled } from './constants';
+import {
+	Event,
+	EventEmitter,
+	ExtensionContext,
+	notebooks,
+	extensions,
+	NotebookEditor,
+} from "vscode";
+import {
+	OpenImageInPlotViewer,
+	SaveImageAs,
+	IsJupyterExtensionInstalled,
+} from "./constants";
 
 export async function activate(context: ExtensionContext): Promise<{
-    onDidReceiveMessage: Event<{ editor: NotebookEditor; message: OpenImageInPlotViewer | SaveImageAs }>;
+	onDidReceiveMessage: Event<{
+		editor: NotebookEditor;
+		message: OpenImageInPlotViewer | SaveImageAs;
+	}>;
 }> {
-    const onDidReceiveMessage = new EventEmitter<{
-        editor: NotebookEditor;
-        message: OpenImageInPlotViewer | SaveImageAs;
-    }>();
-    const messaging = notebooks.createRendererMessaging('jupyter-notebook-renderer');
-    context.subscriptions.push(
-        messaging.onDidReceiveMessage(({ editor, message }) => {
-            const msg = message as OpenImageInPlotViewer | SaveImageAs | IsJupyterExtensionInstalled;
-            if (!msg.type) {
-                return;
-            }
-            if (msg.type === 'isJupyterExtensionInstalled') {
-                void messaging
-                    .postMessage(
-                        <IsJupyterExtensionInstalled>{
-                            type: 'isJupyterExtensionInstalled',
-                            response: !!extensions.getExtension('ms-toolsai.jupyter')
-                        },
-                        editor
-                    )
-                    .then(
-                        (fulfilled) => {
-                            console.log('Sent to UI', fulfilled);
-                        },
-                        (ex) => console.error('Failed to send', ex)
-                    );
-                return;
-            }
-            onDidReceiveMessage.fire({ editor, message: msg });
-        })
-    );
-    messaging.postMessage(<IsJupyterExtensionInstalled>{
-        type: 'isJupyterExtensionInstalled',
-        response: !!extensions.getExtension('ms-toolsai.jupyter')
-    });
+	const onDidReceiveMessage = new EventEmitter<{
+		editor: NotebookEditor;
+		message: OpenImageInPlotViewer | SaveImageAs;
+	}>();
+	const messaging = notebooks.createRendererMessaging(
+		"jupyter-notebook-renderer"
+	);
+	context.subscriptions.push(
+		messaging.onDidReceiveMessage(({ editor, message }) => {
+			const msg = message as
+				| OpenImageInPlotViewer
+				| SaveImageAs
+				| IsJupyterExtensionInstalled;
+			if (!msg.type) {
+				return;
+			}
+			if (msg.type === "isJupyterExtensionInstalled") {
+				void messaging
+					.postMessage(
+						<IsJupyterExtensionInstalled>{
+							type: "isJupyterExtensionInstalled",
+							response:
+								!!extensions.getExtension("ms-toolsai.jupyter"),
+						},
+						editor
+					)
+					.then(
+						(fulfilled) => {
+							console.log("Sent to UI", fulfilled);
+						},
+						(ex) => console.error("Failed to send", ex)
+					);
+				return;
+			}
+			onDidReceiveMessage.fire({ editor, message: msg });
+		})
+	);
+	messaging.postMessage(<IsJupyterExtensionInstalled>{
+		type: "isJupyterExtensionInstalled",
+		response: !!extensions.getExtension("ms-toolsai.jupyter"),
+	});
 
-    return {
-        onDidReceiveMessage: onDidReceiveMessage.event
-    };
+	return {
+		onDidReceiveMessage: onDidReceiveMessage.event,
+	};
 }
