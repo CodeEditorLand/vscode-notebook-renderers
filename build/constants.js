fa723f85a5bdbc871447cb6f96eb547187ba11be
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-12 01:49:04 +0300

    squash!

--- build/constants.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-const path = require('path');
-exports.ExtensionRootDir = path.dirname(__dirname);
-exports.isWindows = /^win/.test(process.platform);
-exports.isCI = process.env.TF_BUILD !== undefined;
